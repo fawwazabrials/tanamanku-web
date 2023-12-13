6ad8148bb91b8b4cd7b8b850031372cfec6d85e6
fawwazabrials <18221067@std.stei.itb.ac.id>
2023-12-13 16:17:47 +0700

    chore: install tailwind & daisyui

---
+++ app/Views/landing.php
@@ -0,0 +1,5 @@
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+    <h1 class="text-red-500 text-xl">This is landing!</h1>
+<?= $this->endSection() ?>
---
+++ app/Views/layout.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tanamanku</title>
+    <link href="<?= base_url()?>/assets/css/styles.css" rel="stylesheet">
+</head>
+<body>
+    <h1>This is layout</h1>
+    <?= $this->renderSection('content') ?>
+</body>
+</html>
